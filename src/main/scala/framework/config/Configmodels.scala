b72f8ca012ca41cd745e7ca8ccec8dcfc1a0e226
Dinesh Jayaraman <40497875+Dinesh8j@users.noreply.github.com>
2026-05-22 17:45:08 +0530

    Update Configmodels.scala

--- src/main/scala/framework/config/Configmodels.scala
+++ src/main/scala/framework/config/Configmodels.scala
@@ -1,15 +1,16 @@
 package framework.config
 
 case class StepConfig(
-                       step:         String,
-                       responseType: Option[String],          // used by: deserialize
-                       packageName:  Option[String],          // used by: invoke
-                       className:    Option[String],          // used by: invoke
-                       method:       Option[String],          // used by: invoke
-                       args:         List[String]             // used by: invoke
-                     )
+  step:               String,
+  responseType:       Option[String],   // deserialize step
+  packageName:        Option[String],   // invoke step
+  className:          Option[String],   // invoke step
+  method:             Option[String],   // invoke step
+  args:               List[String],     // invoke step
+  fetchFromPersistId: Option[Int]       // fetch output of step[index] from store
+)
 
 case class FeatureConfig(
-                          name:  String,
-                          steps: List[StepConfig]
-                        )
+  name:  String,
+  steps: List[StepConfig]
+)
